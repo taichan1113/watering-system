c11a29be147dffe1fef249c9426acb00b8aba8ee
taichan <takecchi.1113@gmail.com>
2022-05-15 21:15:32 +0900

    update

--- service/ServiceEnvironment.py
+++ service/ServiceEnvironment.py
@@ -1,15 +1,18 @@
 from controller.show_tempAndHumid import DHT22
+from controller.show_soilMoisture import CSMS12
 
 class ServiceEnvironment:
   def __init__(self):
     self.temperature = None
     self.humidity = None
     self.soilMoisture = None
-    self.sensor = DHT22()
+    self.sensor_temp_humid = DHT22()
+    self.sensor_soilMoisture = CSMS12()
   
   def serve(self):
-    self.temperature = self.sensor.get_temperature()
-    self.humidity = self.sensor.get_humidity()
+    self.temperature = self.sensor_temp_humid.get_temperature()
+    self.humidity = self.sensor_temp_humid.get_humidity()
+    self.soilMoisture = self.sensor_soilMoisture.moisturePercentage()
 
     pub_msg = f'Temperature: {self.temperature}' + '\n' + f'Humidity: {self.humidity}' + '\n' + f'Soil Moisture: {self.soilMoisture}'
     return pub_msg
