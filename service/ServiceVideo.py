b3343e67958bc0f0b70a5a27182ff0ed0f2d7cc0
taichan <takecchi.1113@gmail.com>
2022-06-13 22:07:30 +0900

    ServiceVideo prototype created and tested

--- service/ServiceVideo.py
+++ service/ServiceVideo.py
@@ -1,22 +1,50 @@
+# from concurrent.futures import thread
+import threading
+import time
+
 class ServiceVideo:
   def __init__(self):
     self.isRecording = False
+    self.event = threading.Event()
 
   def serve(self):
     return
 
   def start(self):
-    return
+    thread_video = threading.Thread(target=self.run)
+    thread_listening = threading.Thread(target=self.stopListening, args = (self.event, ))
+    thread_listening.start()
+    thread_video.start()
 
   def stop(self):
-    return
+    self.event.set()
 
-  
+  def run(self):
+    self.isRecording = True
+    num = 0
+    while self.isRecording:
+      # self.capture() will be here
+      print(num)
+      num = num + 1
+      time.sleep(0.5)
+
+  def stopListening(self, event):
+    event.wait()
+    self.isRecording = False
 
   def getMessage(self):
     return 'video'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   video = ServiceVideo()
   video.start()
+  time.sleep(3)
   video.stop()
+  print('simulation done')
+
+  # event = threading.Event()
+  # thread_video = threading.Thread(target=video.run)
+  # thread_listening = threading.Thread(target=video.stopListening, args = (event, ))
+  
+  # thread_listening.start()
+  # thread_video.start()
