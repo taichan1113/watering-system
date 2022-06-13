3c0e31a5306c85199ff5cb1e214e82d9c2f7ad45
taichan <takecchi.1113@gmail.com>
2022-06-13 20:40:07 +0900

    test

--- service/ServiceVideo.py
+++ service/ServiceVideo.py
@@ -5,5 +5,18 @@ class ServiceVideo:
   def serve(self):
     return
 
+  def start(self):
+    return
+
+  def stop(self):
+    return
+
+  
+
   def getMessage(self):
     return 'video'
+
+if __name__ == "__main__":
+  video = ServiceVideo()
+  video.start()
+  video.stop()
