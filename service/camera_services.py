6c6fbbf6c155a916f99776a1f369d9476537f8ef
taichan <takecchi.1113@gmail.com>
2022-07-31 20:18:11 +0900

    update camera services

--- service/camera_services.py
+++ service/camera_services.py
@@ -1,14 +1,16 @@
+import time
 from service.ServiceVideo import ServiceVideo
 from service.youtube_live import YouTubeLive
 
+
 class CameraServices:
-  def __init__(self):
-    self.video = ServiceVideo()
-    self.stream = YouTubeLive()
-    self.isActive = self.video.isRecording^self.stream.isStreaming # xor
-    
-  def serve(self):
-    return
-  
-  def getMessage(self):
-    return 'test'
+    def __init__(self):
+        self.video = ServiceVideo()
+        self.stream = YouTubeLive()
+        # self.isActive = self.video.isRecording^self.stream.isStreaming # xor
+
+    def serve(self):
+        self.stream.toggle(self.video.isRecording)
+
+    def getMessage(self):
+        return 'test'
--- service/youtube_live.py
+++ service/youtube_live.py
@@ -3,24 +3,30 @@ import subprocess
 import os
 import signal
 
+
 class YouTubeLive():
     def __init__(self):
         self.isStreaming = False
-        # self.DEV_ID = 0
-        # self.WIDTH = 640
-        # self.HEIGHT = 480
-        # self.FPS = 30
         self.cmd = "../controller/Live_v2.sh"
-        
+
     def start(self):
         self.isStreaming = True
 #         self.proc = subprocess.Popen("exec " + self.cmd, shell=True)
-        self.proc = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-        
+        self.proc = subprocess.Popen(
+            self.cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
     def stop(self):
 #         self.proc.kill()
         os.killpg(os.getpgid(self.proc.pid), signal.SIGTERM)
         self.isStreaming = False
+
+    def toggle(self, guard):
+        if self.isStreaming:
+            self.stop()
+        elif not self.isStreaming:
+            while guard:
+                time.sleep(1)
+            self.start()
         
 if __name__ == '__main__':
     live = YouTubeLive()
